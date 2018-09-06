5bbe710a207afbc0738e3a24f0c54520f33f6e1e
Kate Hart <codehearts@users.noreply.github.com>
2018-09-06 13:03:07 -0700

    Use more generic naming for Tuya outlet support

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const TuyaSwitch = require('./tuya_switch.js')
+const TuyaOutlet = require('./tuya_outlet.js')
 const express = require('express');
 const app = express();
 
 const port = 8080;
 const host = '0.0.0.0';
 
-const device = new TuyaSwitch({
+const device = new TuyaOutlet({
   id: 'xxxxxxxxxxxxxxxxxxxx',
   key: 'xxxxxxxxxxxxxxxx',
   ip: 'xxxxxxxxxxxx'});
@@ -22,10 +22,8 @@ app.post('/api', async (req, res) =>{
   if (req.body.dps) {
     const dps = parseInt(req.body.dps, 10);
 
-    console.log(`Toggling dps ${dps}`);
-    await device.toggleOutlet(dps);
-
-    response[dps] = device.getOutletStatus(dps);
+    await device.toggleDps(dps);
+    response[dps] = device.getDpsStatus(dps);
   }
 
   console.log('API call complete');
